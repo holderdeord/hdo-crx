9a924ef9c542bf7212559dff02825034ed8f104d
Jari Bakken <jari@holderdeord.no>
2013-02-16 20:43:56 +0100

    Add a button to open all minutes from the overview page.

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 var snippets = {
-  getSelectionAsHtml: 'var div = document.createElement("div"); div.appendChild(document.getSelection().getRangeAt(0).cloneContents()); div.innerHTML'
+  getSelectionAsHtml: 'var div = document.createElement("div"); div.appendChild(document.getSelection().getRangeAt(0).cloneContents()); div.innerHTML',
+  fetchAllLinks:      'var res = [], nodes = document.querySelectorAll("a"); for (var i=0; i < nodes.length; i++) { res.push(nodes[i].href) }; res;'
 }
 
 function copy(text){
@@ -25,4 +26,22 @@ function copySelectionAsHtml(callback) {
       callback(result[0])
     }
   });
+}
+
+function openAllIssues(callback) {
+  chrome.tabs.executeScript(null, {code: snippets.fetchAllLinks}, function(result) {
+    var opened = false;
+    var hrefs = result[0];
+
+    hrefs.forEach(function(href) {
+      if (href.match(/Referater\/.+\/\d{4}-\d{4}\/(\d{6})\/\d+\/?$/)) {
+        opened = true;
+        chrome.tabs.create({url: href});
+      }
+    });
+
+    if(typeof callback == 'function') {
+      callback(opened)
+    }
+  })
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
 function HdoExtension() {
-  this.statusElement = document.getElementById('status');
+  this.containerElement           = document.body;
+  this.statusElement              = document.getElementById('status');
+  this.copySelectionAsHtmlElement = document.getElementById('copySelectionAsHtml');
+  this.openAllIssuesElement       = document.getElementById('openAllIssues');
 };
 
 HdoExtension.prototype.init = function() {
   var hdo = this;
 
-  document.getElementById('copySelectionAsHtml').addEventListener('click', function() {
+  hdo.copySelectionAsHtmlElement.addEventListener('click', function() {
     hdo.copySelectionAsHtml();
   });
+
+  hdo.openAllIssuesElement.addEventListener('click', function() {
+    hdo.openAllIssues();
+  })
+
+  chrome.tabs.getSelected(null,function(tab) {
+    var match,
+        url = tab.url;
+
+    if (match = url.match(/Referater\/.+\/\d{4}-\d{4}\/(\d{6})\/?$/)) {
+      hdo.openAllIssuesElement.style.display = 'block';
+    }
+  });
+
 };
 
 HdoExtension.prototype.copySelectionAsHtml = function() {
@@ -17,6 +34,13 @@ HdoExtension.prototype.copySelectionAsHtml = function() {
     hdo.setStatus('OK!')
     setTimeout(function() { window.close(); }, 1000)
   });
+};
+
+HdoExtension.prototype.openAllIssues = function() {
+  var hdo = this;
+  chrome.extension.getBackgroundPage().openAllIssues(function() {
+    window.close();
+  })
 };
 
 HdoExtension.prototype.setStatus = function(str) {
